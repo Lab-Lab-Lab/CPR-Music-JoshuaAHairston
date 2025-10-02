788fdb40e80bd0fd7eb66774c1eeb3e39666a15c
Joshua Hairston <hairs3ja@dukes.jmu.edu>
2025-10-02 12:41:41 -0400

    started coding the midi portion in react

---
+++ components/adaptive.js
@@ -0,0 +1,20 @@
+import Button from 'react-bootstrap/Button'
+
+function Adpative() {
+  function enableEverything() {
+    
+  }
+
+  return (
+    <>
+      <Button variant="primary" onClick={enableEverything}>start</Button>
+      <Button variant="primary">Start Recording</Button>
+      <Button variant="primary">End Recording</Button>
+    </>
+  );
+}
+
+
+
+
+export default Adpative;
---
+++ pages/midi.js
@@ -0,0 +1,8 @@
+import Adpative from '../components/adaptive';
+
+function Midi() {
+  return <Adpative></Adpative>;
+}
+
+
+export default Midi;
